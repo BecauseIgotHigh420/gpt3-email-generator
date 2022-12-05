7dea92c7f87ffbd2d98e7590317b26b798f2146c
BecauseIgotHigh420 <92127564+BecauseIgotHigh420@users.noreply.github.com>
2022-12-05 19:45:54 +0100

    ,

--- emailapp.py
+++ emailapp.py
@@ -1,51 +1,75 @@
+import requests 
 import streamlit as st
+from streamlit_lottie import st_lottie
 import openai
 from ml_backend import ml_backend
 
-st.title("Interactive Email Generator App")
-st.text("by Alex Zavalny")
+def load_lottieurl(url):
+    r=requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+#load lottie
+
+lottie_coding = load_lottieurl("https://assets10.lottiefiles.com/packages/lf20_m0D4rj.json")
+
+with st.container() :
+    st.title("Outil de génération de Lettres par IA :pencil:")
+    st.text("by MaLettre.ai - Simo B")
 
 st.markdown(""" 
 
-# About
- 
-## Play around with the sliders and text fields to generate your very own emails! 
-## At the end, you can automatically send your email to a recipient via Gmail  
-
-## Business Benefits and Usecases:
-* Time saved writing medium-long sized emails
-* Mental Energy is conserved
-* Anxiety of writing a **professional sounding** email (or email with any writing style) is removed as the GPT3 Language model used is trained from a variety of many different internet sources
+# À propos
 
 """)
 
-st.markdown("# Generate Email")
+st.write("""Jouez avec les curseurs et vos indications pour générer en quelques secondes vos propres lettres ! 
+À la fin, vous pouvez envoyer automatiquement votre lettre à un destinataire via Gmail""")
+
+with st.container() :
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column: 
+        st.subheader("""
+Bénéfices commerciaux ou personnels et cas d'utilisation:""")
+
+        st.write("""
+- Temps économisé pour écrire des lettres de taille moyenne à longue
+- Allegement de la charge mentale
+- L'anxiété d'écrire une lettre dans n'importe quel style d'écriture est soulevée grâce au modèle de langage d'intélligence artificielle (GPT3) utilisé.
+""")
+
+    with right_column:
+        st_lottie(lottie_coding, height=350, key="coding")
+
+with st.container() :
+    st.write("---")
+    st.markdown("# Générer une lettre")
 
 backend = ml_backend()
 
 with st.form(key="form"):
-    prompt = st.text_input("Describe the Kind of Email you want to be written.")
-    st.text(f"(Example: Write me a professional sounding email to my boss)")
+    prompt = st.text_input("Donnez le plus d'indications possibles concernant la lettre souhaitée")
+    st.text(f"(Example: Rédige moi une lettre sur un ton amicale pour remercier mon voisin de son aide)")
 
-    start = st.text_input("Begin writing the first few or several words of your email:")
+    slider = st.slider("Faites varier le nombre de caractères de votre lettre", min_value=2000, max_value=10000)
 
-    slider = st.slider("How many characters do you want your email to be? ", min_value=64, max_value=750)
-    st.text("(A typical email is usually 100-500 characters)")
 
-    submit_button = st.form_submit_button(label='Generate Email')
+    submit_button = st.form_submit_button(label='Générer la lettre')
 
     if submit_button:
-        with st.spinner("Generating Email..."):
-            output = backend.generate_email(prompt, start)
-        st.markdown("# Email Output:")
-        st.subheader(start + output)
+        with st.spinner("Génération en cours..."):
+            output = backend.generate_email(prompt)
+        st.markdown("# Votre lettre:")
+        st.subheader(output)
 
         st.markdown("____")
-        st.markdown("# Send Your Email")
-        st.subheader("You can press the Generate Email Button again if you're unhappy with the model's output")
+        st.markdown("# Envoyer par email ?")
+        st.subheader("Vous pouvez appuyer à nouveau sur le bouton Générer une lettre si vous n'êtes pas satisfait du résultat du modèle.")
         
-        st.subheader("Otherwise:")
+        st.subheader("Sinon:")
         st.text(output)
-        url = "https://mail.google.com/mail/?view=cm&fs=1&to=&su=&body=" + backend.replace_spaces_with_pluses(start + output)
+        url = "https://mail.google.com/mail/?view=cm&fs=1&to=&su=&body=" + backend.replace_spaces_with_pluses(output)
 
-        st.markdown("[Click me to send the email]({})".format(url))
+        st.markdown("[Cliquez-ici pour l'envoyer]({})".format(url))
--- ml_backend.py
+++ ml_backend.py
@@ -2,16 +2,16 @@ import openai
 
 class ml_backend:
         
-    openai.api_key = 'YOUR-API-KEY-HERE'
+    openai.api_key = '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'
 
-    def generate_email(self, userPrompt ="Write me a professionally sounding email", start="Dear"):
-        """Returns a generated an email using GPT3 with a certain prompt and starting sentence"""
+    def generate_email(self, userPrompt ="Rédige moi une lettre", start=""):
+        """Renvoie une lettre générée en utilisant l'intelligence artificielle"""
 
         response = openai.Completion.create(
-        engine="davinci",
+        engine="text-davinci-003",
         prompt=userPrompt + "\n\n" + start,
         temperature=0.71,
-        max_tokens=150,
+        max_tokens=2049,
         top_p=1,
         frequency_penalty=0.36,
         presence_penalty=0.75
@@ -19,7 +19,7 @@ class ml_backend:
         return response.get("choices")[0]['text']
 
     def replace_spaces_with_pluses(self, sample):
-        """Returns a string with each space being replaced with a plus so the email hyperlink can be formatted properly"""
+        """Renvoie une chaîne de caractères avec chaque espace remplacé par un plus afin que le lien d'email puisse être formaté correctement"""
         changed = list(sample)
         for i, c in enumerate(changed):
             if(c == ' ' or c =='  ' or c =='   ' or c=='\n' or c=='\n\n'):
